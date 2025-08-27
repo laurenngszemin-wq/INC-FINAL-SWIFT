1aaeb0917ec5db6e4f81ddb2d98116a6d48d4261
Lauren <lauren_ng_sze_min@s2025.ssts.edu.sg>
2025-08-27 21:22:23 +0800

    Initial Commit

---
+++ INC FINAL SWIFT/INC_FINAL_SWIFTApp.swift
@@ -0,0 +1,17 @@
+//
+//  INC_FINAL_SWIFTApp.swift
+//  INC FINAL SWIFT
+//
+//  Created by Lauren Ng Sze Min on 27/8/25.
+//
+
+import SwiftUI
+
+@main
+struct INC_FINAL_SWIFTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
