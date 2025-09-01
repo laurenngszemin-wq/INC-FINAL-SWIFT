4056e158f4e085367bfad0a8406d22081eebff4e
sidkdvdj <aaron_justin_quah@s2025.ssts.edu.sg>
2025-09-01 17:25:41 +0800

    Update Organism.swift

--- INC FINAL SWIFT/Organism.swift
+++ INC FINAL SWIFT/Organism.swift
@@ -6,25 +6,52 @@
 import SwiftUI
 
 struct Organism: View {
+    @State private var showingSheet = false
+    @State private var Password = ["hound", "restless maw"]
+    @State private var newtitle = ""
+    
     var body: some View {
         NavigationView {
             VStack {
-                Text("""
-                     Accursed ____, whose hunger knows no close,
-                     With ______, devours both bread and bone.
-                     So dire his craving, wealth to hunger goes—
-                     His master ____, and conscience is o’erthrown.
-
-
-                     For every ____, thy master bends and breaks,
-                     To __________ hath no end.
-                     All honor lost, each wretched choice he makes,
-                     For the ____, his soul must now ____.
-                     """)
-
-                .fontWeight(.bold)
                 
+                Text("""
+                 Accursed ____, whose hunger knows no close, with ______  ______, devours both bread and bone.
+                 """)
+                
+                .fontWeight(.bold)
+                .font(.title3)
+                
+                .toolbar{
+                    ToolbarItem(placement: .topBarTrailing) {
+                        
+                        Button{
+                            showingSheet = true
+                        } label: {
+                            Text("Enter")
+                                .font(.title2)
+                                .fontWeight(.bold)
+                        }
+                        
+                    }
+                }
+                
+
+                .sheet(isPresented: $showingSheet) {
+                    VStack(spacing: 20) { Text("Enter the word")
+                            .font(.title2)
+                            .bold()
+                        Form{
+                            TextField("Enter the word", text: $newtitle)
+                        }
+                    }
+                }
             }
+        }
+    }
+    
+    struct Organism_Previews: PreviewProvider {
+        static var previews: some View {
+            Organism()
         }
     }
 }
