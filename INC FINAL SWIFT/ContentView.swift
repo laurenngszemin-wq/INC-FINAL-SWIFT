ecf30a232e9bdb4ba68069b85cf9d96c14eda280
sidkdvdj <aaron_justin_quah@s2025.ssts.edu.sg>
2025-09-08 15:21:04 +0800

    Update ContentView.swift
    Hi, sidkdvdj

--- INC FINAL SWIFT/ContentView.swift
+++ INC FINAL SWIFT/ContentView.swift
@@ -4,36 +4,51 @@
 //
 //  Created by Lauren Ng Sze Min on 27/8/25.
 //
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationView {
-            VStack {
-                
-                Text("The INCoin Heist test")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .foregroundColor(.blue)
-                List {
-                    NavigationLink(destination: Part1() ) {
-                        Text("1. Steps")
-                            .font(.title)
-                            .fontWeight(.bold)
+        TabView {
+            
+            // Tab 1: Current menu / tasks
+            NavigationView {
+                VStack {
+                    Text("The INCoin Heist test")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(.blue)
+                        .padding()
                     
+                    List {
+                        NavigationLink(destination: Part1()) {
+                            Text("Tasks")
+                                .font(.title)
+                                .fontWeight(.bold)
+                        }
                     }
-                    }
-              
+                    .listStyle(PlainListStyle())
                 }
-                .listStyle(PlainListStyle())
-                .padding()
             }
+            .tabItem {
+                Image(systemName: "house.fill")
+                Text("Home")
+                    .font(.largeTitle)
+            }
+            
+            Culprits()
+                .tabItem {
+                    Image(systemName: "person.fill.questionmark")
+                    Text("Culprits")
+                        .font(.largeTitle)
+                }
         }
     }
-
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
